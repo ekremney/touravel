6c174676a138bd0080d78c35f14942c4721ddbc1
Gökhan Güler <gokguler5@gmail.com>
2015-04-13 01:04:31 +0300

    Map integration is succesful.

--- android/app/src/main/java/com/example/touravel/app/MapActivity.java
+++ android/app/src/main/java/com/example/touravel/app/MapActivity.java
@@ -8,15 +8,25 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.SupportMapFragment;
 
-public class MapActivity extends ActionBarActivity {
+
+public class MapActivity extends ActionBarActivity implements OnMapReadyCallback {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -32,10 +42,18 @@ public class MapActivity extends ActionBarActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        if(id == R.id.action_settings) {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        LatLng bilkent = new LatLng(39.874615, 32.747596);
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(bilkent, 13));
+        map.addMarker(new MarkerOptions().position(bilkent).title("İlk Konum"));
     }
 
 }
