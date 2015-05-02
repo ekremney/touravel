cec2204ecc5ea2c652a9e84e32c5ca177bc65b38
Ekrem Doğan <ekremney@gmail.com>
2015-05-02 20:02:20 +0300

    webservice project structure improved

--- webservices/__init__.py
+++ webservices/__init__.py
@@ -1,20 +1,2 @@
 #!/usr/bin/env python
 from wsgi import application as app
-from main import db
-from flask.ext.script import Manager, Shell
-from flask.ext.migrate import Migrate, MigrateCommand
-from main.models.user import User
-
-manager = Manager(app)
-migrate = Migrate(app, db)
-
-manager.add_command('db', MigrateCommand)
-
-def make_shell_context():
-	return dict(app=app, db=db, User=User)
-
-manager.add_command("shell", Shell(make_context=make_shell_context))
-
-
-if __name__ == '__main__':
-    manager.run()
--- webservices/wsgi.py
+++ webservices/wsgi.py
@@ -1,7 +1,22 @@
 import sys
 sys.path.insert(0, '/home/narlab/webapps/touravel')
-from main import create_app
-application = create_app('development')
+from main import create_app, db
+from flask.ext.script import Manager, Shell
+from flask.ext.migrate import Migrate, MigrateCommand
+from main.models.user import User
+
+
+application = create_app('production')
+
+manager = Manager(application)
+migrate = Migrate(application, db)
+
+manager.add_command('db', MigrateCommand)
+
+def make_shell_context():
+	return dict(app=application, db=db, User=User)
+
+manager.add_command("shell", Shell(make_context=make_shell_context))
 
 
 @application.route("/")
@@ -9,4 +24,4 @@ def hello():
     return "It Works!"
 
 if __name__ == '__main__':
-	application.run()
+	manager.run()
